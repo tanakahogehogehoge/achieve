38c5cca249adcf9a253b1694b5fa798053123a7a
Yuta <ny_bigapplejr@yahoo.co.jp>
2017-07-08 14:59:00 +0000

    20170708twelve

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
                password_confirmation: password,
                )
 end
+
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああタイトル",
+    content: "hoge内容",
+    user_id: n
+  )
+  n = n + 1
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
