4a3f4cbd286232efb8a8cf0e3ee6668a2f0e0851
Yuta <ny_bigapplejr@yahoo.co.jp>
2017-07-08 15:05:49 +0000

    20170709first

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               )
-end
+#100.times do |n|
+  #email = Faker::Internet.email
+  #password = "password"
+  #User.create!(email: email,
+   #            password: password,
+  #             password_confirmation: password,
+ #              )
+#end
 
 n = 1
 while n <= 100
